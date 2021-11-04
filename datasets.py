a99bfa6e28a99966d39442672003c8433d814081
Marek Vasko <xvasko16@vutbr.cz>
2021-11-04 19:31:18 +0100

    Changes to be more compliant with expected inputs/outputs

--- datasets.py
+++ datasets.py
@@ -6,14 +6,14 @@ class Dataset1(VisionDataset):
     def __init__(
         self, 
         root: str, 
-        split: str = "train",
+        image_set: str = "train",
         transforms: Optional[Callable] = None, 
         transform: Optional[Callable] = None, 
         target_transform: Optional[Callable] = None
     ) -> None:
         super().__init__(root, transforms=transforms, transform=transform, target_transform=target_transform)
         
-        if split not in ("train", "test"):
+        if image_set not in ("train", "test"):
             assert "err"
         
         self.objs = list(filter(os.path.isdir, [os.path.join(root, d) for d in os.listdir(root)]))
@@ -41,7 +41,7 @@ class Dataset1(VisionDataset):
         
         targets = []
         for i, t in enumerate(self.target_type):
-            target = Image.open(self.targets[index][i])
+            target = Image.open(self.targets[index][i]).convert('P')
             targets.append(target)
 
         target = tuple(targets) if len(targets) > 1 else targets[0]
@@ -59,7 +59,7 @@ class Dataset2(VisionDataset):
     def __init__(
         self, 
         root: str, 
-        split: str = "train",
+        image_set: str = "train",
         transforms: Optional[Callable] = None, 
         transform: Optional[Callable] = None, 
         target_transform: Optional[Callable] = None
@@ -71,12 +71,12 @@ class Dataset2(VisionDataset):
             "test" : "b. Testing Set"
         }
 
-        if split not in dataset_path:
+        if image_set not in dataset_path:
             assert "err"
 
 
-        self.images = os.listdir(os.path.join(root, "A. Segmentation", "1. Original Images", dataset_path[split] ) )
-        self.images = [ os.path.join(root, "A. Segmentation", "1. Original Images", dataset_path[split], f) for f in self.images]
+        self.images = os.listdir(os.path.join(root, "A. Segmentation", "1. Original Images", dataset_path[image_set] ) )
+        self.images = [ os.path.join(root, "A. Segmentation", "1. Original Images", dataset_path[image_set], f) for f in self.images]
         self.images.sort()
 
         self.targets = {
@@ -88,8 +88,8 @@ class Dataset2(VisionDataset):
         }
 
         for t in self.targets:
-            self.targets[t] = os.listdir( os.path.join(root , "A. Segmentation" , "2. All Segmentation Groundtruths" , dataset_path[split] , t) )
-            self.targets[t] = [ os.path.join(root , "A. Segmentation" , "2. All Segmentation Groundtruths" , dataset_path[split] , t, f) for f in self.targets[t]]
+            self.targets[t] = os.listdir( os.path.join(root , "A. Segmentation" , "2. All Segmentation Groundtruths" , dataset_path[image_set] , t) )
+            self.targets[t] = [ os.path.join(root , "A. Segmentation" , "2. All Segmentation Groundtruths" , dataset_path[image_set] , t, f) for f in self.targets[t]]
             self.targets[t].sort()
 
     def __getitem__(self, index: int) -> Tuple[Any, Any]:
@@ -104,7 +104,7 @@ class Dataset2(VisionDataset):
 
         targets = []
         for t in self.targets:
-            target = Image.open(self.targets[t][index])
+            target = Image.open(self.targets[t][index]).convert('P')
             targets.append(target)
 
         target = tuple(targets) if len(targets) > 1 else targets[0]
